add0382bcff6b843e1805bf8979892a84a401e96
josemourinho333 <yoo.phil92@gmail.com>
2022-08-01 13:33:20 -0700

    Add update to product spec with more precise testing

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -8,33 +8,37 @@ RSpec.describe Product, type: :model do
       it "return true when saved correctly''" do
         @category = Category.create
         @product = Product.create(name: 'Phil', price: '200', quantity: 2, category: @category)
-        expect(@product).to be_present
+        expect(@product).to be_valid
       end
     end
 
     describe 'Product model validations' do
       it 'returns true when given name is nil' do
         @category = Category.create
-        @product = Product.create(name: nil)
-        expect(@product.name).to be_nil
+        @product = Product.create(price: '200', quantity: 2, category: @category)
+        expect(@product).to_not be_valid
+        expect(@product.errors.full_messages).to include("Name can't be blank")
       end
 
       it 'returns true when given price is nil' do
         @category = Category.create
         @product = Product.create(name: 'Phil', quantity: 2, category: @category)
-        expect(@product.price).to be_nil
+        expect(@product).to_not be_valid
+        expect(@product.errors.full_messages).to include("Price can't be blank")
       end
       
       it 'returns true when given quantity is nil' do
         @category = Category.create
         @product = Product.create(name: 'Phil', price: 200, category: @category)
-        expect(@product.quantity).to be_nil
+        expect(@product).to_not be_valid
+        expect(@product.errors.full_messages).to include("Quantity can't be blank")
       end
 
       it 'returns true when given category is nil' do
         @category = Category.create
         @product = Product.create(name: 'Phil', price: 200, quantity: 3)
-        expect(@product.category).to be_nil
+        expect(@product).to_not be_valid
+        expect(@product.errors.full_messages).to include("Category can't be blank")
       end
     end
 
