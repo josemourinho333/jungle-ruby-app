14a7b7914bf684376b21c022e8ea1ffe86b3af16
josemourinho333 <yoo.phil92@gmail.com>
2022-08-01 17:16:42 -0700

    Add new validation and spec for user auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,20 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 3 }
+  validates :password_digest, presence: true
+
+  def self.authenticate_with_credentials(email_address, password)
+    @user = User.find_by_email(email_address.strip.downcase)
+
+    if @user && @user.authenticate(password)
+      @user
+    else
+      nil
+    end
+  end
+
 end
